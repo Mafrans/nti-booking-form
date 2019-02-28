988614c82a5ee371a546800bc2013098cd516e9b
BuildTools <unconfigured@null.spigotmc.org>
2019-03-01 00:25:06 +0100

    Fixes for chrome users running on AM/PM time

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
 $(function() {
 });
 
-$(".from input[type='time']").keypress(function () {
-
-    console.log($("button#reset-button.datetime-reset-button"));
-    $("#reset-button").hide();
-});
-
 function select(button) {
     console.log(button);
     if(!$(button).hasClass("selected")) {
